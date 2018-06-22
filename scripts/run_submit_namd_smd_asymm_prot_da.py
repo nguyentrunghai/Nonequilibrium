15033d4a084e953794a4c8b7418288079bbded19
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-06-22 15:50:54 -0500

    modify run_submit_namd_smd_asymm_prot_da.py

--- scripts/run_submit_namd_smd_asymm_prot_da.py
+++ scripts/run_submit_namd_smd_asymm_prot_da.py
@@ -14,17 +14,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument( "--first_job_index",   type=int, default=0)
 parser.add_argument( "--last_job_index",    type=int, default=5)
 
-parser.add_argument( "--initial_pdb_dir",   type=str, default="equilibrate_at_13A")
+parser.add_argument( "--initial_pdb_dir",   type=str, default="equilibrate_at_33A")
 parser.add_argument( "--in_pdb_prefix",     type=str, default="conf_")
 
 parser.add_argument( "--structure_file",  type=str, default="da.psf")
 parser.add_argument( "--parm_file",       type=str, default="par_all27_prot_lipid_cmap.prm")
 
-parser.add_argument( "--forward_tcl",       type=str, default="/home/tnguye46/nonequilibrium/ALA_Namd/tcl_force/forward_20A_per_20ps.tcl")
-parser.add_argument( "--backward_tcl",      type=str, default="/home/tnguye46/nonequilibrium/ALA_Namd/tcl_force/backward_20A_per_20ps.tcl")
+parser.add_argument( "--tcl_file",   type=str,
+                     default="/home/tnguye46/nonequilibrium/ALA_Namd/tcl_force/backward_20A_per_20ps.tcl")
 
-parser.add_argument( "--output_forward",     type=str, default="forward")
-parser.add_argument( "--output_backward",    type=str, default="backward")
+
+parser.add_argument( "--output",     type=str, default="backward")
 
 parser.add_argument( "--temperature",  type=float, default=300)
 
@@ -47,26 +47,15 @@ for i, pdb_file in pdb_files:
 
     out_dir = os.path.abspath(out_dir)
 
-    namd_forward = os.path.join(out_dir, args.output_forward + ".namd")
+    namd_conf = os.path.join(out_dir, args.output + ".namd")
 
-    write_namd_conf_smd_da(pdb_file, args.structure_file, args.parm_file, args.output_forward,
-                           args.temperature, args.forward_tcl, args.steps,
-                           namd_forward,
+    write_namd_conf_smd_da(pdb_file, args.structure_file, args.parm_file, args.output,
+                           args.temperature, args.tcl_file, args.steps,
+                           namd_conf,
                            coordinates_res=None,
                            velocities_res=None)
 
-
-    namd_backward = os.path.join(out_dir, args.output_backward + ".namd")
-    coor_res = os.path.join(out_dir, args.output_forward + ".restart.coor")
-    vel_res = os.path.join(out_dir, args.output_forward + ".restart.vel")
-
-    write_namd_conf_smd_da(pdb_file, args.structure_file, args.parm_file, args.output_backward,
-                           args.temperature, args.backward_tcl, args.steps,
-                           namd_backward,
-                           coordinates_res=coor_res,
-                           velocities_res=vel_res)
-
-    qsub_log = os.path.join(out_dir, "logfile")
+    qsub_log = os.path.join(out_dir, args.output + ".log")
 
     qsub_text = head_text_4_qsub_script("cpu", qsub_log, mem=2048)
     qsub_text += "\n"
@@ -75,15 +64,9 @@ for i, pdb_file in pdb_files:
     qsub_text += "cd " + out_dir + "\n"
     qsub_text += "\n"
 
-    log_forward = os.path.join(out_dir, args.output_forward + ".log")
-    qsub_text += "namd2 " + namd_forward + " > " + log_forward + "\n"
-    qsub_text += "echo DONE FORWARD\n"
+    qsub_text += "namd2 " + namd_conf + "\n"
+    qsub_text += "echo DONE\n"
     qsub_text += "date\n\n"
-
-    log_backward = os.path.join(out_dir, args.output_backward + ".log")
-    qsub_text += "namd2 " + namd_backward + " > " + log_backward + "\n"
-    qsub_text += "echo DONE BACKWARD\n"
-    qsub_text += "date\n"
 
     qsub_script = os.path.join(out_dir, "smd.job")
     open(qsub_script, "w").write(qsub_text)
