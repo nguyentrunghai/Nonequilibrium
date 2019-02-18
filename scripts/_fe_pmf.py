a6bb470fa41cb4b45a760b39aa69e12dff6c2315
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-02-18 14:36:59 +0700

    add function unidirectional_pmf() to _fe_pmf.py

--- scripts/_fe_pmf.py
+++ scripts/_fe_pmf.py
@@ -28,7 +28,10 @@ def unidirectional_fe(pulling_data, nblocks, ntrajs_per_block,
         raise ValueError("Number of trajs requested is too large")
 
     free_energies = {}
+    free_energies["nblocks"] = nblocks
+    free_energies["ntrajs_per_block"] = ntrajs_per_block
     free_energies["timeseires_indices"] = timeseires_indices
+    
     free_energies["ks"] = pulling_data["ks"]
     free_energies["dt"] = pulling_data["dt"]
 
@@ -49,7 +52,62 @@ def unidirectional_fe(pulling_data, nblocks, ntrajs_per_block,
         for block in range(nblocks):
             left_bound = block * ntrajs_per_block
             right_bound = (block + 1) * ntrajs_per_block
-            free_energies["bootstrap_%d"%bootstrap]["block_%d"%block] = uni_df_t(w_t_bootstrap[left_bound : right_bound])
+            free_energies["bootstrap_%d" % bootstrap]["block_%d" % block] = uni_df_t(
+                w_t_bootstrap[left_bound : right_bound])
 
     return free_energies
+
+
+def unidirectional_pmf(pulling_data,
+                       nblocks, ntrajs_per_block,
+                       pmf_bin_edges, V,
+                       nbootstraps=0):
+    """
+    :param pulling_data:
+    :param pmf_bin_edges:
+    :param V:
+    :param nblocks:
+    :param ntrajs_per_block:
+    :param nbootstraps:
+    :return:
+    """
+    total_ntrajs_in_data = pulling_data["wF_t"].shape[0]
+    total_ntrajs_requested = nblocks * ntrajs_per_block
+    if total_ntrajs_requested > total_ntrajs_in_data:
+        raise ValueError("Number of trajs requested is too large")
+
+    lambda_F = pulling_data["lambda_F"]
+    ks = pulling_data["ks"]
+    w_t = pulling_data["wF_t"][:total_ntrajs_requested]
+    z_t = pulling_data["zF_t"][:total_ntrajs_requested]
+
+    pmfs = {}
+    pmfs["nblocks"] = nblocks
+    pmfs["ntrajs_per_block"] = ntrajs_per_block
+    pmfs["pmf_bin_edges"] = pmf_bin_edges
+    pmfs["ks"] = ks
+
+    pmfs["main_estimates"] = {}
+    for block in range(nblocks):
+        left_bound = block * ntrajs_per_block
+        right_bound = (block + 1) * ntrajs_per_block
+        pmfs["main_estimates"]["block_%d" % block] = uni_pmf(z_t[left_bound: right_bound],
+                                                             w_t[left_bound: right_bound],
+                                                             lambda_F, V, ks, pmf_bin_edges)
+
+    for bootstrap in range(nbootstraps):
+        pmfs["bootstrap_%d" % bootstrap] = {}
+        traj_indices = np.random.choice(total_ntrajs_requested, size=total_ntrajs_requested, replace=True)
+        w_t_bootstrap = w_t[traj_indices]
+        z_t_bootstrap = z_t[traj_indices]
+
+        for block in range(nblocks):
+            left_bound = block * ntrajs_per_block
+            right_bound = (block + 1) * ntrajs_per_block
+
+            pmfs["bootstrap_%d" % bootstrap]["block_%d" % block] = uni_pmf(z_t_bootstrap[left_bound: right_bound],
+                                                             w_t_bootstrap[left_bound: right_bound],
+                                                             lambda_F, V, ks, pmf_bin_edges)
+
+    return pmfs
 
