a80f322aad8528b6a9263ee132f1c06ffc984336
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-02-19 13:05:32 +0700

    wrote symmetric_fe() and symmetric_pmf()

--- scripts/_fe_pmf.py
+++ scripts/_fe_pmf.py
@@ -279,3 +279,117 @@ def bidirectional_pmf(pulling_data,
                                                                 lambda_F, V, ks, pmf_bin_edges)
 
     return pmfs
+
+
+def symmetric_fe(pulling_data, nblocks, ntrajs_per_block,
+                      timeseries_indices,
+                      nbootstraps=0):
+    """
+    :param pulling_data: dict returned by _IO.load_1d_sim_results()
+    :param nblocks: int, number of blocks of trajectories
+    :param ntrajs_per_block: int, number of trajectories per block
+    :param timeseries_indices: list or 1d ndarray of int
+    :param nbootstraps: int, number of bootstrap samples
+    :return: free_energies, dict
+    """
+    if ntrajs_per_block % 2 != 0:
+        raise ValueError("Number of trajs per block must be even")
+
+    total_ntrajs_in_data = pulling_data["wF_t"].shape[0]
+
+    total_ntrajs_requested = nblocks * ntrajs_per_block
+    if total_ntrajs_requested > total_ntrajs_in_data:
+        raise ValueError("Number of trajs requested is too large")
+
+    free_energies = {}
+    free_energies["nblocks"] = nblocks
+    free_energies["ntrajs_per_block"] = ntrajs_per_block
+    free_energies["timeseries_indices"] = timeseries_indices
+
+    free_energies["ks"] = pulling_data["ks"]
+    free_energies["dt"] = pulling_data["dt"]
+
+    w_t = pulling_data["wF_t"][: total_ntrajs_requested, timeseries_indices]
+    lambdas = pulling_data["lambda_F"][timeseries_indices]
+    free_energies["lambdas"] = lambdas
+
+    free_energies["main_estimates"] = {}
+    for block in range(nblocks):
+        left_bound = block * ntrajs_per_block
+        right_bound = (block + 1) * ntrajs_per_block
+        free_energies["main_estimates"]["block_%d" % block] = sym_est_df_t_v1(w_t[left_bound: right_bound])
+
+    for bootstrap in range(nbootstraps):
+        free_energies["bootstrap_%d"%bootstrap] = {}
+        traj_indices = np.random.choice(total_ntrajs_requested, size=total_ntrajs_requested, replace=True)
+        w_t_bootstrap = w_t[traj_indices]
+
+        for block in range(nblocks):
+            left_bound = block * ntrajs_per_block
+            right_bound = (block + 1) * ntrajs_per_block
+            free_energies["bootstrap_%d" % bootstrap]["block_%d" % block] = sym_est_df_t_v1(
+                w_t_bootstrap[left_bound : right_bound])
+
+    return free_energies
+
+
+def symmetric_pmf(pulling_data,
+                  nblocks, ntrajs_per_block,
+                  pmf_bin_edges, V,
+                  symmetrize_pmf,
+                  nbootstraps=0):
+    """
+    :param pulling_data: dict returned by _IO.load_1d_sim_results()
+    :param nblocks: int, number of blocks of trajectories
+    :param ntrajs_per_block: int, number of trajectories per block
+    :param pmf_bin_edges: ndarray, bin edges
+    :param V: python function, pulling harmonic potential
+    :param symmetrize_pmf: bool
+    :param nbootstraps: int, number of bootstrap samples
+    :return: pmfs, dict
+    """
+    if ntrajs_per_block % 2 != 0:
+        raise ValueError("Number of trajs per block must be even")
+
+    total_ntrajs_in_data = pulling_data["wF_t"].shape[0]
+
+    total_ntrajs_requested = nblocks * ntrajs_per_block
+    if total_ntrajs_requested > total_ntrajs_in_data:
+        raise ValueError("Number of trajs requested is too large")
+
+    lambdas = pulling_data["lambda_F"]
+    w_t = pulling_data["wF_t"][: total_ntrajs_requested]
+    z_t = pulling_data["zF_t"][: total_ntrajs_requested]
+
+    pmfs = {}
+    pmfs["nblocks"] = nblocks
+    pmfs["ntrajs_per_block"] = ntrajs_per_block
+    pmfs["pmf_bin_edges"] = pmf_bin_edges
+    pmfs["ks"] = ks
+
+    pmfs["main_estimates"] = {}
+    for block in range(nblocks):
+        left_bound = block * ntrajs_per_block
+        right_bound = (block + 1) * ntrajs_per_block
+        _, pmfs["main_estimates"]["block_%d" % block] = sym_est_pmf_v1(z_t[left_bound: right_bound],
+                                                                       w_t[left_bound: right_bound],
+                                                                       lambdas, V, ks, pmf_bin_edges,
+                                                                       symmetrize_pmf)
+
+    for bootstrap in range(nbootstraps):
+        pmfs["bootstrap_%d" % bootstrap] = {}
+        traj_indices = np.random.choice(total_ntrajs_requested, size=total_ntrajs_requested, replace=True)
+        w_t_bootstrap = w_t[traj_indices]
+        z_t_bootstrap = z_t[traj_indices]
+
+        for block in range(nblocks):
+            left_bound = block * ntrajs_per_block
+            right_bound = (block + 1) * ntrajs_per_block
+
+            _, pmfs["bootstrap_%d" % bootstrap]["block_%d" % block] = sym_est_pmf_v1(
+                z_t_bootstrap[left_bound: right_bound],
+                w_t_bootstrap[left_bound: right_bound],
+                lambdas, V, ks, pmf_bin_edges,
+                symmetrize_pmf)
+
+    return pmfs
