1a7285d4d1a198779944be959e74cef002ee5801
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-02-28 10:16:20 +0700

    add option want_right_replicate_for_asym

--- scripts/run_plot_fe_pmf_1d.py
+++ scripts/run_plot_fe_pmf_1d.py
@@ -38,6 +38,8 @@ parser.add_argument("--pmf_ylabel", type=str, default="$\Phi(z)$")
 # for asymmetric data plot pmf from pmf[bin_ind_to_start_to_plot] to pmf[len]
 parser.add_argument("--bin_ind_to_start_to_plot", type=int, default=1)
 
+parser.add_argument( "--want_right_replicate_for_asym", action="store_true", default=False)
+
 parser.add_argument("--legend_ncol", type=int, default=3)
 
 parser.add_argument("--xlimits_fe", type=str, default="None")
@@ -185,8 +187,9 @@ for file, label in zip(free_energies_pmfs_files, data_estimator_pairs):
     data = _put_first_or_min_to_zero(data)
 
     # replica data on the right side
-    if label in ["f_u", "r_u", "fr_b"]:
-        data = _right_replicate_data(data, args.system_type)
+    if args.want_right_replicate_for_asym:
+        if label in ["f_u", "r_u", "fr_b"]:
+            data = _right_replicate_data(data, args.system_type)
 
     fe_x = data["free_energies"]["lambdas"]
     fe_ys = np.array(data["free_energies"]["main_estimates"].values())
@@ -195,8 +198,7 @@ for file, label in zip(free_energies_pmfs_files, data_estimator_pairs):
     free_energies[label] = {"x":fe_x, "y":fe_y, "error":fe_error}
 
     pmf_x = bin_centers(data["pmfs"]["pmf_bin_edges"])
-    # TODO
-    print(label, data["pmfs"]["pmf_bin_edges"].shape)
+    #print(label, data["pmfs"]["pmf_bin_edges"].shape)
     pmf_ys = np.array(data["pmfs"]["main_estimates"].values())
     pmf_y = pmf_ys.mean(axis=0)
     pmf_error = pmf_ys.std(axis=0)
@@ -253,33 +255,26 @@ plot_lines(xs, ys, yerrs=yerrs,
 
 
 # plot pmfs
-# TODO
 start_pmf_ind = args.bin_ind_to_start_to_plot
 
 xs = []
 ys = []
 yerrs = []
 for label in data_estimator_pairs:
-    if label in ["s_u", "s_b", "s_s"]:
-        end_pmf_ind = len(pmfs[label]["x"]) - start_pmf_ind
-    else:
+    end_pmf_ind = len(pmfs[label]["x"]) - start_pmf_ind
+
+    if label in ["f_u", "r_u", "fr_b"] and (not args.want_right_replicate_for_asym):
         end_pmf_ind = len(pmfs[label]["x"])
+
     xs.append(pmfs[label]["x"][start_pmf_ind : end_pmf_ind])
     ys.append(pmfs[label]["y"][start_pmf_ind : end_pmf_ind])
     yerrs.append(pmfs[label]["error"][start_pmf_ind : end_pmf_ind])
-    #xs.append(pmfs[label]["x"])
-    #ys.append(pmfs[label]["y"])
-    #yerrs.append(pmfs[label]["error"])
 
 
 end_pmf_ind = len(pmf_exact["pmf"]) - start_pmf_ind
 xs.append(bin_centers(pmf_exact["pmf_bin_edges"])[start_pmf_ind : end_pmf_ind])
 ys.append(pmf_exact["pmf"][start_pmf_ind : end_pmf_ind])
 yerrs.append(None)
-
-#xs.append(bin_centers(pmf_exact["pmf_bin_edges"]))
-#ys.append(pmf_exact["pmf"])
-#yerrs.append(None)
 
 if args.xlimits_pmf.lower() != "none":
     xlimits_pmf = [float(s) for s in args.xlimits_pmf.split()]
