29936a34b6e2f390633a3220522260541dacf7f3
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-03-18 14:15:39 +0700

    fix bug

--- scripts/utils.py
+++ scripts/utils.py
@@ -294,7 +294,7 @@ def stride_lambda_indices(lambda_F, lambda_R, n):
 
 
 def _allclose_where(scalar, array, threshold=1e-5):
-    assert np.abs(array[1] - array[1]) < threshold, "threshold is too big"
+    assert np.nanmean(np.abs(array[1:] - array[:-1])) < threshold, "threshold is too big"
     replicated = np.array([scalar] * array.shape[0])
     near_zero = np.abs(array - replicated)
     near_zero = np.where(near_zero < threshold)
