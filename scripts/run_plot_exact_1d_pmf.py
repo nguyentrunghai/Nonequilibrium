d2bb2753c83fdc13e18bc73230cb4d8d65b20ad3
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-04-08 16:14:48 +0700

    calculate exact pmf

--- scripts/run_plot_exact_1d_pmf.py
+++ scripts/run_plot_exact_1d_pmf.py
@@ -2,9 +2,26 @@
 from __future__ import division
 from __future__ import print_function
 
+import argparse
+
 import numpy as np
 from utils import bin_centers
 from models_1d import U0_sym, U0_asym
+
+parser = argparse.ArgumentParser()
+
+# symmetric or asymmetric
+parser.add_argument( "--system_type", type=str, default="symmetric")
+
+parser.add_argument("--left_most_edge", type=float, default=-1.5)
+parser.add_argument("--right_most_edge", type=float, default=1.5)
+
+# number of bins for the PMF
+parser.add_argument("--pmf_nbins", type=int, default=40)
+
+parser.add_argument("--out", type=str, default="exact_pmf.pdf")
+
+args = parser.parse_args()
 
 
 def _exact_pmf(system_type, pmf_bin_edges):
@@ -19,3 +36,10 @@ def _exact_pmf(system_type, pmf_bin_edges):
     exact_pmf = U0(centers)
 
     return centers, exact_pmf
+
+
+pmf_bin_edges = np.linspace(args.left_most_edge, args.right_most_edge, args.pmf_nbins + 1)
+bin_centers, exact_pmf = _exact_pmf(args.system_type, pmf_bin_edges)
+
+
+
