23d3f343d3e19cf099176152ea25881964610ab4
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-06-13 13:26:15 +0700

    write function _index_closest_to

--- scripts/run_plot_Van_pmf_rmse.py
+++ scripts/run_plot_Van_pmf_rmse.py
@@ -1,2 +1,19 @@
 """
+plot rmse of pmf data from Van A Ngo from Los Alamos
 """
+from __future__ import print_function
+from __future__ import division
+
+import numpy as np
+
+def _index_closest_to(ref_array, query_number):
+    """
+    find the index of an element of ref_array that is closest to query_number
+    :param ref_array: 1D array
+    :param query_number: float
+    :return: idx, int
+    """
+    assert ref_array.ndim == 1, "ref_array must be 1D"
+    abs_diff = np.abs(ref_array - query_number)
+    idx = abs_diff.argmin()
+    return idx
