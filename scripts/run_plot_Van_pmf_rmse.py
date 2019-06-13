4414f52ad6cd119835cc8b2c80ec64ceff9cf9b4
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-06-13 13:46:13 +0700

    write function _index_closest_to

--- scripts/run_plot_Van_pmf_rmse.py
+++ scripts/run_plot_Van_pmf_rmse.py
@@ -6,6 +6,9 @@ from __future__ import division
 
 import numpy as np
 
+from _fe_pmf_plot_utils import min_to_zero
+
+
 def _index_closest_to(ref_array, query_number):
     """
     find the index of an element of ref_array that is closest to query_number
@@ -17,3 +20,15 @@ def _index_closest_to(ref_array, query_number):
     abs_diff = np.abs(ref_array - query_number)
     idx = abs_diff.argmin()
     return idx
+
+
+def _indices_closest_to(ref_array, query_array):
+    """
+    find a list of indices of elements in ref_array that are each closest to each element in query_array
+    :param ref_array: 1D array
+    :param query_array: 1D array
+    :return: indices, list
+    """
+    indices = [_index_closest_to(ref_array, test) for test in query_array]
+    return indices
+
