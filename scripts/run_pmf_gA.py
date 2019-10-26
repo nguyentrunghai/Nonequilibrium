a75599d422ec91a0cf558e82c6ee782e4f9858ad
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-10-26 22:46:20 +0700

    load work data

--- scripts/run_pmf_gA.py
+++ scripts/run_pmf_gA.py
@@ -7,10 +7,23 @@ from __future__ import print_function
 import argparse
 
 import numpy as np
+import netCDF4 as nc
+
+from estimators import uni_pmf, bi_pmf, sym_est_pmf_v1
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--work_data_file", type=str, default="work.nc")
+
+# lower and upper edges are taken from pmf_min_max/
+parser.add_argument( "--pmf_lower_edge",            type=float, default=1)
+parser.add_argument( "--pmf_upper_edge",            type=float, default=2)
+
+# number of bins for the PMF
+parser.add_argument( "--pmf_nbins", type=int, default=20)
+
+# some number or -999 (means None)
+parser.add_argument( "--symmetric_center",   type=float, default=-999)
 
 args = parser.parse_args()
 
@@ -31,3 +44,15 @@ def _pmf_bin_edges(lower, upper, nbins, symmetric_center):
     bin_edges = np.linspace(lower, upper, nbins + 1)
 
     return bin_edges
+
+
+if args.symmetric_center == -999:
+    symmetric_center = None
+else:
+    symmetric_center = args.symmetric_center
+bin_edges = _pmf_bin_edges(args.pmf_lower_edge, args.pmf_upper_edge, args.pmf_nbins, symmetric_center)
+
+
+with nc.Dataset(args.work_data_file, "r") as handle:
+    data = {key: handle.variables[key][:] for key in handle.variables.keys()}
+
