2bab2c2b615ed03af99ebfb61fc94ec3cc172191
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-10-26 16:24:55 +0700

    add function _convert_2_acc_work()

--- scripts/run_convert_transfered_to_accumulated_work_gA.py
+++ scripts/run_convert_transfered_to_accumulated_work_gA.py
@@ -4,6 +4,9 @@ to convert transfered to accumulated work for gA system
 
 import argparse
 
+from models_1d import V
+from _IO import save_to_nc
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--work_in_file", type=str, default="work.nc")
 
@@ -11,4 +14,19 @@ parser.add_argument("--force_constant", type=float, default=100)
 
 parser.add_argument("--work_out_file", type=str, default="accumulated_work.nc")
 args = parser.parse_args()
+
+
+def _convert_2_acc_work(w_t, z_t, lambda_t, k):
+    """
+    use Eq. (31) in Hummer and Szabo 2005
+    :param w_t: 2d array of shape (ntrajs, times)
+    :param z_t: 2d array of shape (ntrajs, times)
+    :param lambda_t: 1d array of shape (times,)
+    :param k: float, pulling force constant
+    :return: acc_work
+    """
+    v_t = V(z_t, k, lambda_t)       # v_t has shape (ntrajs, times)
+    v_0 = v_t[:, [0]]               # v_0 has shape (ntrajs, 1)
+    acc_w_t = w_t + v_t - v_0
+    return acc_w_t
 
