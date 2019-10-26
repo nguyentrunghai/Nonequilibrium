e12fef4d41cbea0f0c7155f18d8c4a60b22d7621
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-10-26 16:59:20 +0700

    note on unit of force constant

--- scripts/run_convert_transfered_to_accumulated_work_gA.py
+++ scripts/run_convert_transfered_to_accumulated_work_gA.py
@@ -12,7 +12,7 @@ from _IO import save_to_nc
 parser = argparse.ArgumentParser()
 parser.add_argument("--work_in_file", type=str, default="work.nc")
 
-parser.add_argument("--force_constant", type=float, default=100)
+parser.add_argument("--force_constant", type=float, default=100)   # kcal/mol/A^2
 
 parser.add_argument("--work_out_file", type=str, default="accumulated_work.nc")
 args = parser.parse_args()
